4429df36b970197d3e917f5684152fa2cab032fd
crhatigan <crhatigan28@stsaviour.org>
2025-11-20 12:40:17 -0500

    lab 3

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import random
 import time
+import sys
 
 from draw import draw_d20, draw_d6, draw_d4
 
-def print_dramatic_text(text: str, delay=0.1):
+def print_dramatic_text(text: str, delay=0.05):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
+
+def check_player_alive(lives: int) -> None:
+    if lives <= 0:
+        print_dramatic_text('You died')
+        sys.exit()
 
 if __name__ == '__main__':
     # create character by collecting user input (name + class)
@@ -16,12 +22,49 @@ if __name__ == '__main__':
     # any buffs / debuffs?
     # any critical success / failure?
 
-    name = input('Name: ')
-    role = input('Role: ')
+    print_dramatic_text('Welcome to my random trivia game!')
 
-    print('Your name is ' + name + ' and your role is ' + role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    # the player starts with three lives and loses one for each wrong answer
+    lives = 3
+    answer = input('What is 12 times 6? ')
+    if answer == '72':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
+    answer = input('What is the fastest animal in the world? ')
+    if answer.lower() == 'falcon':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
+    answer = input('What color is the sky? ')
+    if answer.lower() == 'blue':
+        print_dramatic_text('correct')
+    else:
+        print_dramatic_text('Incorrect')
+        lives -= 1
+        check_player_alive(lives)
+    
+    questions = [
+        'What is the largest planet?',
+        'What is the slowest animal in the world?'
+    ]
 
-    input('Press Enter to roll a d20.')
-    roll = random.randint(1, 20)
-    draw_d20(roll)
+    answers = [
+        'jupiter',
+        'banana slug'
+    ]
+
+    position = random.randint(0, 1)
+    answer = input(questions[position] + ' ')
+    if answer.lower() == answers[position]:
+        print_dramatic_text('correct')
+    else:
+        lives -= 1
+        check_player_alive(lives)
+        print_dramatic_text('incorrect')
+
+    print_dramatic_text('Congratulations you have completed the trivia game!')
