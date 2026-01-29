2a53a28be87c2d0b1a46e20409bef258613aef14
crhatigan <crhatigan28@stsaviour.org>
2026-01-29 13:37:35 -0500

    https://github.com/crhatigan10/st-saviour-foundations-fall-2025.git

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flower import Flower
 
 if __name__ == '__main__':
+    # here is my first flower example
     tulip = Flower('Tulip', 6, True)
     print(tulip.bloom())
-
+    # here is my second flower example
     daisy = Flower('Daisy', 8, False)
     print(daisy.bloom())
